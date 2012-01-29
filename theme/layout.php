dfbc941e66850731cf9dca07b5f3182562216089
ReimuHakurei <reimu@reimuhakurei.net>
2012-01-28 19:34:01 -0600

    GoodbIE 6 (Removed all IE 6 hacks)

--- theme/layout.php
+++ theme/layout.php
@@ -6,25 +6,28 @@
 
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="viewport" content="width=device-width, user-scalable=true, initial-scale=1, maximum-scale=1">
-		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">		
-		
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">        
+        
         <link href="resources/css/bootstrap.min.css" rel="stylesheet">
         <link href="resources/css/main.css" rel="stylesheet">
         <link href="resources/css/bbcode.css" rel="stylesheet">
 
         <script type="text/javascript" src="resources/js/jquery-1.7.1.min.js"></script>
         <script type="text/javascript" src="resources/js/bootstrap-dropdown.js"></script>
-		
+        
         <?php
             if ($NeptuneCore->var_get("output","menu_active") != "") {
                 echo '<style type="text/css">#' . $NeptuneCore->var_get("output","menu_active") . '{background-color:#222;background-color:rgba(0, 0, 0, 0.5);}</style>' . "\n";
             }
         ?>
                     
-        <!-- Hacks to get this to work in IE 6-IE 8 -->
+        <!-- Hacks to get this to work in IE 7 and IE 8
+             Not placed in a conditional comment because these are also useful for other non-IE browsers (such as Firefox 2.x) 
+        -->
+        <!--[if gte IE 7]><!-->
         <script type="text/javascript" src="resources/js/html5.js"></script>
-		<?php if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) { echo '
-        <!--[if lte IE 9]>
+        <?php if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) { echo '
+        <!--[if gte IE 7]>
             <script type="text/javascript">
                 $(function() {
                     if(!$.support.placeholder) { 
@@ -46,19 +49,10 @@
                     }
                 });
             </script>
-        <![endif]-->
-		<!--[if lte IE 6]>
-			<script type="text/javascript" src="resources/ie/IE7.js">var IE7_PNG_SUFFIX=".png";</script>
-			<style type="text/css">
-				#mobile-menu { display: none; }
-				ul, li { zoom: 1; }
-				li.divider { display: inline; height: 0px; }
-				body { background-color: white; }
-			</style>
-        <![endif]-->';}?>
-		<script type="text/javascript" src="resources/js/respond.min.js"></script>
+        <![endif]-->';}?><script type="text/javascript" src="resources/js/respond.min.js"></script>
+        <!--<![endif]-->
     </head>
-    <body onload="$('#mobile-menu').dropdown();$('#user-menu').dropdown();">		
+    <body onload="$('#mobile-menu').dropdown();$('#user-menu').dropdown();">        
         <div class="topbar">
             <div class="fill">
                 <div class="container">
@@ -107,18 +101,21 @@
                             <div class="iewarning">Warning: Please upgrade your browser to something compatible with the internet.<br><a href="http://www.browserchoice.eu/" target="_blank">There are many browsers to choose from, any except the one you are using is good.</a></div>
                             <br><br>
                         <![endif]-->
-						<!--[if lte IE 6]>
-							<div class="iewarning">
-								<h2>Unsupported Browser</h2>
-								<p>You are using an <b>extremely outdated, unsupported browser</b>.</p>
-								<p><a href="http://www.browserchoice.eu/" target="_blank">Please <b>keep it real</b> and use a browser that isn\'t <b>over 10 years old</b>.</a></p>
-							</div>
-						<![endif]-->';}?><h2><?php echo $NeptuneCore->var_get("output","title"); ?></h2>
+                        <!--[if lte IE 6]>
+                            <div class="iewarning">
+                                <h2>Unsupported Browser</h2>
+                                <p>You are using an <b>extremely outdated, unsupported browser</b>.</p>
+                                <p><a href="http://www.browserchoice.eu/" target="_blank">Please <b>keep it real</b> and use a browser that isn\'t <b>over 10 years old</b>.</a></p>
+                            </div>
+                        <![endif]-->';}?><h2><?php echo $NeptuneCore->var_get("output","title"); ?></h2>
                         <?php
                             if ($NeptuneCore->var_get("output","subtitle") != "") {
                                 echo "<p><small>" . $NeptuneCore->var_get("output","subtitle") . "</small></p>\n";
                             }
-                        ?><hr><?php echo "\n" . $NeptuneCore->var_get("output","body") . "\n"; ?>
+                        ?>
+
+                        <hr>
+                        <?php echo "\n" . $NeptuneCore->var_get("output","body") . "\n"; ?>
                     </div>
                 </div>
                 <footer>
