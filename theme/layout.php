50029712f2385df1264affae7d8a5de6226172e6
Alex Ingram <reimu@reimuhakurei.net>
2012-05-25 18:02:27 -0500

    Fix iOS zooming bug on text boxes.

--- theme/layout.php
+++ theme/layout.php
@@ -10,7 +10,7 @@
         
 		
 		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 		<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">	      
 		
 		<link href="resources/css/bbcode.css" rel="stylesheet">
