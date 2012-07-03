4214a4d6b4569a7d823bd3678027fd5380662389
Alex Ingram <reimu@reimuhakurei.net>
2012-07-02 21:48:02 -0500

    Example MySQL config file and gitignore file.

---
+++ system/config/mysql.example.php
@@ -0,0 +1,13 @@
+<?php
+	/*
+		Neptune Content Management System
+		MySQL Config - /system/config/mysql.php
+
+		Stores MySQL configuration.
+	*/
+
+	$conf['database']['host'] = 'localhost';
+	$conf['database']['user'] = 'username';
+	$conf['database']['pass'] = 'password';
+	$conf['database']['db'] = 'your_database';
+?>
