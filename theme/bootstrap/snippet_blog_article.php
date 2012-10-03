0abffa2675ee7249158db39f0e8c48c92817d43f
Alex Ingram <reimu@reimuhakurei.net>
2012-10-03 00:10:24 -0500

    This is the start of a Blog module. When this is finished, ReimuCMS will officially be completely useless.

---
+++ theme/bootstrap/snippet_blog_article.php
@@ -0,0 +1,20 @@
+<?php
+				if (@$notFirstArticle) {
+					$NeptuneCore->var_append("output","blog-body",'<hr>');
+				} else {
+					$notFirstArticle = true;
+				}
+				$NeptuneCore->var_append("output","blog-body",'<div class="content-area blog-article">
+                    <h2>' . $NeptuneCore->var_get("output","title_prepend") . $NeptuneCore->var_get("output","title") . $NeptuneCore->var_get("output","title_append") . '</h2>');
+                    if ($NeptuneCore->var_get("output","subtitle") != "") {
+                        $NeptuneCore->var_append("output","blog-body","<p><small>" . $NeptuneCore->var_get("output","subtitle") . "</small></p>\n");
+                    }
+                    $NeptuneCore->var_append("output","blog-body","\n" . $NeptuneCore->var_get("output","body") . "\n" . ' 
+                </div>'); 
+				
+				$NeptuneCore->var_clear("output","title_prepend");
+				$NeptuneCore->var_clear("output","title");
+				$NeptuneCore->var_clear("output","title_append");
+				$NeptuneCore->var_clear("output","subtitle");
+				$NeptuneCore->var_clear("output","body");
+				?>
