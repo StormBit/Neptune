5c0f50506a9bbc12de8552a85b91d6ccfc480357
Alex "Antoligy" Wilson <antoligy@antoligy.com>
2012-10-10 14:19:25 +0100

    Corrections to German locale
    Credit to SignZ

--- system/locale/de.php
+++ system/locale/de.php
@@ -1,17 +1,18 @@
 ﻿<?php
 	// Locale: German
+	// Credit to SignZ
 	
 	// User account stuff
 	$conf['locale']['login']			= "Anmelden";
 	$conf['locale']['logout']			= "Abmelden";
-	$conf['locale']['register']			= "Anmelden";
-	$conf['locale']['createaccount']	= "Anmelden";
-	$conf['locale']['username']			= "Nutzername";
+	$conf['locale']['register']			= "Registrieren";
+	$conf['locale']['createaccount']	= "Account erstellen";
+	$conf['locale']['username']			= "Benutzername";
 	$conf['locale']['password']			= "Passwort";
 	$conf['locale']['passwordconfirm']	= "Passwort wiederholen";
 	$conf['locale']['emailoptional']	= "E-Mail-Adresse";
-	$conf['locale']['usernametaken']	= "Benutzername genommen.";
-	$conf['locale']['mismatchedpass']	= "Deine passwort stimmt nicht überein.";
+	$conf['locale']['usernametaken']	= "Benutzername vergeben..";
+	$conf['locale']['mismatchedpass']	= "Die Passwörter stimmen nicht überein.";
 	$conf['locale']['loginfailed']		= "Anmeldung fehlgeschlagen.";
 	$conf['locale']['baduserpass']		= "Benutzername und Passwort stimmen nicht überein.";
 	
