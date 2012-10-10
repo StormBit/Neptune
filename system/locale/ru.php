5192bc9033e7b388190991d7c04e76133021f161
Alex "Antoligy" Wilson <antoligy@antoligy.com>
2012-10-10 13:49:47 +0100

    Added German and Russian locales, updated README.
    - Added Russian locale (corrections pending).
    - Added German locale (corrections pending).
    - Updated README.md with more information.
    - Updated .gitignore to ignore .bak files.
    - Added .gitattributes to avoid any future issues.

---
+++ system/locale/ru.php
@@ -0,0 +1,22 @@
+﻿<?php
+	// Locale: Russian
+	
+	// User account stuff
+	$conf['locale']['login']			= "Вход в систему";
+	$conf['locale']['logout']			= "Выйти";
+	$conf['locale']['register']			= "Зарегистрироваться.";
+	$conf['locale']['createaccount']	= "Зарегистрировать.";
+	$conf['locale']['username']			= "Логин.";
+	$conf['locale']['password']			= "Придумайте пароль.";
+	$conf['locale']['passwordconfirm']	= "Подтвердите пароль.";
+	$conf['locale']['emailoptional']	= "Электронная почта";
+	$conf['locale']['usernametaken']	= "Логин для регистрации недоступен.";
+	$conf['locale']['mismatchedpass']	= "Пароли не совпадают.";
+	$conf['locale']['loginfailed']		= "Авторизоваться не удалось.";
+	$conf['locale']['baduserpass']		= "Неправильная пара логин-пароль.";
+	
+	// Dropdown menu stuff
+	$conf['locale']['editprofile']		= "Edit Profile";
+	$conf['locale']['ucp']				= "User Control Panel";
+	$conf['locale']['acp']				= "Admin Control Panel";
+?>
