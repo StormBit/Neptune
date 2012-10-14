29f57b206c54b1cb90407c28dad444833a5f2f71
Alex Ingram <reimu@reimuhakurei.net>
2012-10-13 21:02:32 -0500

    I have no clue why this is even still here, nor do I remember why it was added in the first place. Some code change at some point seems to have broken it anyway, so I am removing it.

--- system/modules/blank.php
+++
@@ -1,7 +0,0 @@
-<?php
-	function mod_blank_blank() { 
-		global $NeptuneCore; 
-		$NeptuneCore->var_set("theme","altlayout","layout_blank"); 
-    }
-    $NeptuneCore->hook_function("blank","blank","blank");
-?>
