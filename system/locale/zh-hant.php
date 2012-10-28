9151b3d3f8fb32a2252e4525fc2fbbbe11d047b6
Alex Ingram <reimu@reimuhakurei.net>
2012-10-28 15:36:54 -0500

    Added Simplified Chinese and Traditional Chinese translations, courtesy of Xaojan.

---
+++ system/locale/zh-hant.php
@@ -0,0 +1,24 @@
+﻿<?php
+	// Locale: Traditional Chinese
+	// Translated by Xaojan
+	
+	// User account stuff
+	$conf['locale']['login']			= "登錄";
+	$conf['locale']['logout']			= "註銷";
+	$conf['locale']['register']			= "註冊";
+	$conf['locale']['createaccount']	= "創建帳戶";
+	$conf['locale']['username']			= "用戶名";
+	$conf['locale']['password']			= "密碼";
+	$conf['locale']['passwordconfirm']	= "確認密碼";
+	$conf['locale']['emailoptional']	= "郵箱 (可選)";
+	$conf['locale']['usernametaken']	= "該用戶名被佔用了。";
+	$conf['locale']['mismatchedpass']	= "兩次密碼不一致。";
+	$conf['locale']['loginfailed']		= "登錄失敗";
+	$conf['locale']['baduserpass']		= "用戶名或者密碼錯誤。";
+	$conf['locale']['ie6useraccounts']	= "用戶管理頁面不支持IE6，請更新您的流覽器。";
+	
+	// Dropdown menu stuff
+	$conf['locale']['editprofile']		= "修改資料";
+	$conf['locale']['ucp']				= "用戶控制面板";
+	$conf['locale']['acp']				= "管理面板";
+?>
