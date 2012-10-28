862745744f0ab6f61e1104046299b8a01b6fd77c
ReimuHakurei <reimu@reimuhakurei.net>
2012-10-28 15:36:54 -0500

    Added Simplified Chinese and Traditional Chinese translations, courtesy of Xaojan.

---
+++ system/locale/zh-hans.php
@@ -0,0 +1,24 @@
+﻿<?php
+	// Locale: Simplified Chinese
+	// Translated by Xaojan
+	
+	// User account stuff
+	$conf['locale']['login']			= "登录";
+	$conf['locale']['logout']			= "注销";
+	$conf['locale']['register']			= "注册";
+	$conf['locale']['createaccount']	= "创建账户";
+	$conf['locale']['username']			= "用户名";
+	$conf['locale']['password']			= "密码";
+	$conf['locale']['passwordconfirm']	= "确认密码";
+	$conf['locale']['emailoptional']	= "邮箱 (可选)";
+	$conf['locale']['usernametaken']	= "该用户名被占用了。";
+	$conf['locale']['mismatchedpass']	= "两次密码不一致。";
+	$conf['locale']['loginfailed']		= "登录失败";
+	$conf['locale']['baduserpass']		= "用户名或者密码错误。";
+	$conf['locale']['ie6useraccounts']	= "用户管理页面不支持IE6，请更新您的浏览器。";
+	
+	// Dropdown menu stuff
+	$conf['locale']['editprofile']		= "修改资料";
+	$conf['locale']['ucp']				= "用户控制面板";
+	$conf['locale']['acp']				= "管理面板";
+?>
