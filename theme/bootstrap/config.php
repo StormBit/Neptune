a9f84e2a63c3eaf15b4c49f1f0bd68bab4e02f02
Karen Tsai <angelXwind@angelxwind.net>
2014-02-05 18:49:04 -0500

    Added show-login to the Neptune-Bootstrap configuration.

--- theme/bootstrap/config.php
+++ theme/bootstrap/config.php
@@ -2,5 +2,6 @@
 	// Configuration file for the Neptune-Bootstrap theme.
 	
 	$conf['theme']['inverse-navbar'] = false; // If this is set to true, the navbar will be black. If it is set to false, it will be white.
+	$conf['theme']['show-login'] = true; // If this is set to true, the login/register buttons will be shown on the navbar.
 	$conf['theme']['subtitle'] = false; // If this is set to true, a subtitle will be displayed on all pages containing the page creation and last-modified dates.
 ?>
