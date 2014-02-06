00da627ae400ec84d5ae35210b74d2f4bd4d8f02
Karen Tsai <angelXwind@angelxwind.net>
2014-02-06 16:56:13 -0500

    One does not simply translate English to English.

--- system/locale/en.php
+++ system/locale/en.php
@@ -11,28 +11,28 @@
 	$conf['locale']['passwordconfirm']			= "Password (confirm)";
 	$conf['locale']['emailoptional']			= "Email (optional)";
 	$conf['locale']['usernametaken']			= "Username already taken.";
-	$conf['locale']['usernameempty']			= "No username specified."; // Needs translation.
+	$conf['locale']['usernameempty']			= "No username specified.";
 	$conf['locale']['mismatchedpass']			= "Passwords do not match.";
 	$conf['locale']['loginfailed']				= "Login Failed";
 	$conf['locale']['baduserpass']				= "Incorrect username and/or password.";
 	$conf['locale']['ie6useraccounts']			= "User Accounts are not supported in Internet Explorer 6. Please update your web browser.";
-	$conf['locale']['noemail']					= "No Email address entered."; // Needs translation.
+	$conf['locale']['noemail']					= "No Email address entered.";
 
 	// Various errors
-	$conf['locale']['accessdenied']				= "Access Denied"; // Needs translation.
-	$conf['locale']['nopermission']				= "You do not have permission to view this page."; // Needs translation.
-	$conf['locale']['404module']				= "404 Module Not Found"; // Needs translation.
-	$conf['locale']['404moduletext']			= "Your request could not be processed, because the specified module does not exist."; // Needs translation.
-	$conf['locale']['attemptedtoloadconfig']	= "A core function or module attempted to load the configuration file "; // Needs translation. The filename of the config file will appear after that space
-	$conf['locale']['attemptedtoloadconfig2']	= ", but the file does not exist."; // Needs translation. The filename of the config file will appear before that comma
-	$conf['locale']['missingtheme']				= "The theme file specified in the system configuration is missing."; // Needs translation.
+	$conf['locale']['accessdenied']				= "Access Denied";
+	$conf['locale']['nopermission']				= "You do not have permission to view this page.";
+	$conf['locale']['404module']				= "404 Module Not Found";
+	$conf['locale']['404moduletext']			= "Your request could not be processed, because the specified module does not exist.";
+	$conf['locale']['attemptedtoloadconfig']	= "A core function or module attempted to load the configuration file ";
+	$conf['locale']['attemptedtoloadconfig2']	= ", but the file does not exist.";
+	$conf['locale']['missingtheme']				= "The theme file specified in the system configuration is missing.";
 
 	// Footer!
-	$conf['locale']['modulesloaded']			= "Modules loaded: "; // Needs translation.
-	$conf['locale']['footergentime']			= "Page generated in "; // Needs translation.
-	$conf['locale']['footergentime2']			= " ms with "; // Needs translation.
-	$conf['locale']['footersql']				= " queries and "; // Needs translation.
-	$conf['locale']['footerram']				= " of RAM"; // Needs translation.
+	$conf['locale']['modulesloaded']			= "Modules loaded: ";
+	$conf['locale']['footergentime']			= "Page generated in ";
+	$conf['locale']['footergentime2']			= " ms with ";
+	$conf['locale']['footersql']				= " queries and ";
+	$conf['locale']['footerram']				= " of RAM";
 
 	// Dropdown menu stuff
 	$conf['locale']['editprofile']				= "Edit Profile";
